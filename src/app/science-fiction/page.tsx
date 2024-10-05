1b61d6de5db4899f4037f74c0f22b1c74ec05315
Maurice de Beijer <maurice.de.beijer@gmail.com>
2024-10-05 10:51:14 +0200

    Fetching data in a RSC

--- src/app/science-fiction/page.tsx
+++ src/app/science-fiction/page.tsx
@@ -1,29 +1,15 @@
 import { GridLayout } from '@/components/grid-layout'
 import { MovieCard } from '@/components/movie-card'
+import { prisma } from '@/lib/prisma'
 
-export default function ScienceFictionMovies() {
-  const movies = [
-    {
-      favourite: true,
-      id: 693134,
-      overview:
-        'Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.',
-      posterPath: '/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg',
-      title: 'Dune: Part Two',
-      voteAverage: 8.2,
-      voteCount: 5080,
-    },
-    {
-      favourite: false,
-      id: 1091,
-      overview:
-        'In the winter of 1982, a twelve-man research team at a remote Antarctic research station discovers an alien buried in the snow for over 100,000 years. Soon unfrozen, the form-changing creature wreaks havoc, creates terror... and becomes one of them.',
-      posterPath: '/tzGY49kseSE9QAKk47uuDGwnSCu.jpg',
-      title: 'The Thing',
-      voteAverage: 8.057,
-      voteCount: 6788,
-    },
-  ]
+export default async function ScienceFictionMovies() {
+  const movies = await prisma.genre
+    .findUniqueOrThrow({ where: { id: 878 } })
+    .movies({
+      orderBy: {
+        title: 'asc',
+      },
+    })
 
   return (
     <div className="p-4">
