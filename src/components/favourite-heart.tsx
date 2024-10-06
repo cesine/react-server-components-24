4c422777566d8bc3f1b613a1040e4e438a85c9b2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2024-10-06 16:32:48 +0200

    Client components

--- src/components/favourite-heart.tsx
+++ src/components/favourite-heart.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Heart } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
@@ -14,6 +16,9 @@ export function FavouriteHeart({ favourite, movieId }: Props) {
       className={cn('cursor-pointer text-green-500', {
         'fill-green-500': favourite,
       })}
+      onClick={() => {
+        console.log(`We want to add this movie to favourites: ${movieId}`)
+      }}
     />
   )
 }
