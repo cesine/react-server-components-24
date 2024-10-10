3817fdbeb88c97f882a946b7fd0fab8e3eb23fbd
Maurice de Beijer <maurice.de.beijer@gmail.com>
2024-10-10 16:54:36 +0200

    The useFormStatus() hook

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -2,5 +2,11 @@ import { useFormStatus } from 'react-dom'
 import { Button } from './ui/button'
 
 export function SubmitButton({ children }: React.PropsWithChildren) {
-  return <Button type="submit">{children}</Button>
+  const { pending } = useFormStatus()
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {children}
+    </Button>
+  )
 }
