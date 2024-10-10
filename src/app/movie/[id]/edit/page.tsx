b87acdb567b53f4a08640156215c3d8a9b061f51
Maurice de Beijer <maurice.de.beijer@gmail.com>
2024-10-10 16:26:09 +0200

    Form actions

--- src/app/movie/[id]/edit/page.tsx
+++ src/app/movie/[id]/edit/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 import { MovieEditor } from '@/components/movie-editor'
 import { handleSubmitMovieForm } from '@/server/actions'
 import { getMovie } from '@/server/movie'
+import { prisma } from '@/lib/prisma'
 
 type Props = {
   params: Promise<{ id: string }>
@@ -17,9 +18,24 @@ export default async function MovieEditPage(props: Props) {
     redirect('/404')
   }
 
+  const formAction = async (formData: FormData) => {
+    'use server'
+    const json = Object.fromEntries(formData.entries())
+    console.log('Form submit', json)
+
+    movie.title = formData.get('title') as string
+    movie.overview = formData.get('overview') as string
+    // Etc.
+
+    await prisma.movie.update({
+      where: { id: movie.id },
+      data: movie,
+    })
+  }
+
   return (
     <div className="p-4">
-      <MovieEditor movie={movie} />;
+      <MovieEditor movie={movie} formAction={formAction} />;
     </div>
   )
 }
