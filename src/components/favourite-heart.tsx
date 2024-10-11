aab1fa1a9214978acf1c063b0338cba99ebd0ff4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2024-10-11 17:06:49 +0200

    The useOptimistic() hook

--- src/components/favourite-heart.tsx
+++ src/components/favourite-heart.tsx
@@ -4,6 +4,7 @@ import { Heart } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 import { toggleFavourite } from '@/server/actions'
+import { useOptimistic, useTransition } from 'react'
 
 type Props = {
   favourite: boolean
@@ -11,18 +12,25 @@ type Props = {
 }
 
 export function FavouriteHeart({ favourite, movieId }: Props) {
+  const [isPending, startTransition] = useTransition()
+  const [optimisticFavourite, setOptimisticFavourite] = useOptimistic(favourite)
+
   return (
     <Heart
       aria-label={favourite ? 'Remove from favourites' : 'Add to favourites'}
       className={cn('cursor-pointer text-green-500', {
-        'fill-green-500': favourite,
+        'fill-green-500': optimisticFavourite,
+        'opacity-50': isPending,
       })}
-      onClick={async () => {
-        try {
-          await toggleFavourite(movieId)
-        } catch (error) {
-          console.error(error)
-        }
+      onClick={() => {
+        startTransition(async () => {
+          try {
+            setOptimisticFavourite(!optimisticFavourite)
+            await toggleFavourite(movieId)
+          } catch (error) {
+            console.error(error)
+          }
+        })
       }}
     />
   )
